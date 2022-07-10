5c74f97a871904b0f201822501ea10f285f3068b
Mael FOSSO <fosso.mael.elvis@gmail.com>
2022-07-10 01:24:04 +0100

    updated the Server by adding an instantiation function

--- server/server.go
+++ server/server.go
@@ -1,9 +1,12 @@
 package server
 
 import (
+	"net"
 	"net/http"
+	"strconv"
+	"time"
 
-	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/v5"
 	"go.uber.org/zap"
 )
 
@@ -18,4 +21,27 @@ type Options struct {
 	Host string
 	Log  *zap.Logger
 	Port int
+}
+
+func New(opts Options) *Server {
+	if opts.Log == nil {
+		opts.Log = zap.NewNop()
+	}
+
+	address := net.JoinHostPort(opts.Host, strconv.Itoa(opts.Port))
+	mux := chi.NewMux()
+
+	return &Server{
+		address: address,
+		log:     opts.Log,
+		mux:     mux,
+		server: &http.Server{
+			Addr:              address,
+			Handler:           mux,
+			ReadTimeout:       5 * time.Second,
+			ReadHeaderTimeout: 5 * time.Second,
+			WriteTimeout:      5 * time.Second,
+			IdleTimeout:       5 * time.Second,
+		},
+	}
 }
