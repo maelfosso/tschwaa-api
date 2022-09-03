3f87885ee0946e1dd091a9f8542eccdf751fdd86
Mael FOSSO <fosso.mael.elvis@gmail.com>
2022-09-03 09:52:37 +0100

    added cors to support local webapp

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.18
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/go-chi/chi/v5 v5.0.7 // indirect
+	github.com/go-chi/cors v1.2.1
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.12.1 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
--- server/routes.go
+++ server/routes.go
@@ -1,12 +1,21 @@
 package server
 
 import (
+	"github.com/go-chi/cors"
 	"tschwaa.com/api/handlers"
 )
 
 type signupperMock struct{}
 
 func (s *Server) setupRoutes() {
+	s.mux.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:3000"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "PATCH"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
 	handlers.Health(s.mux)
 
 	// Auth
