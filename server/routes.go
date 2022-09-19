29916ae314a7e9aead1fca1ef68ea84f65bc36eb
Mael FOSSO <fosso.mael.elvis@gmail.com>
2022-09-19 08:23:24 +0100

    added middleware for logging request received

--- server/routes.go
+++ server/routes.go
@@ -1,15 +1,37 @@
 package server
 
 import (
+	"net/http"
+	"time"
+
 	"github.com/go-chi/cors"
+	"go.uber.org/zap"
 	"tschwaa.com/api/handlers"
 )
 
 type signupperMock struct{}
 
+type statusResponseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func NewStatusResponseWriter(w http.ResponseWriter) *statusResponseWriter {
+	return &statusResponseWriter{
+		ResponseWriter: w,
+		statusCode:     http.StatusOK,
+	}
+}
+
+func (srw *statusResponseWriter) WriteHeader(statusCode int) {
+	srw.statusCode = statusCode
+	srw.ResponseWriter.WriteHeader(statusCode)
+}
+
 func (s *Server) setupRoutes() {
+	s.mux.Use(s.requestLoggerMiddleware)
 	s.mux.Use(cors.Handler(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:3000", "http://www.tschwaa.local"},
+		AllowedOrigins:   []string{"*"}, // http://localhost:3000", "http://www.tschwaa.local"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "PATCH"},
 		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
 		ExposedHeaders:   []string{"Link"},
@@ -25,4 +47,25 @@ func (s *Server) setupRoutes() {
 
 	// Organization
 	handlers.CreateOrganization(s.mux, s.database)
+}
+
+func (s *Server) requestLoggerMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		start := time.Now()
+		srw := NewStatusResponseWriter(w)
+
+		defer func() {
+			s.log.Info(
+				"Request sent",
+				zap.String("method", req.Method),
+				zap.Duration("started at", time.Since(start)),
+				zap.Int("status", srw.statusCode),
+				zap.String("host", req.Host),
+				zap.String("path", req.URL.Path),
+				zap.String("query", req.URL.RawQuery),
+			)
+		}()
+
+		next.ServeHTTP(srw, req)
+	})
 }
