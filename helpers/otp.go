c539b4b4e25fd8fbda676c54c0a1a81f852a223e
Mael FOSSO <fosso.mael.elvis@gmail.com>
2023-08-06 01:14:00 +0100

    [auth-otp] created otp helpers

---
+++ helpers/otp.go
@@ -0,0 +1,30 @@
+package helpers
+
+import (
+	"math/rand"
+	"time"
+)
+
+func createSeed(now time.Time) *rand.Rand {
+	return rand.New(
+		rand.NewSource(
+			now.UnixNano(),
+		),
+	)
+}
+
+func stringWithCharset(now time.Time, charset string, length int) string {
+	seed := createSeed(now)
+
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[seed.Intn(len(charset))]
+	}
+
+	return string(b)
+}
+
+func GenerateOTP(now time.Time) string {
+	charset := "0123456789"
+	return stringWithCharset(now, charset, 4)
+}
