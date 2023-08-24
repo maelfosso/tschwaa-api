58ba100f6577e9a64fcc5c50ae520e6778137ec8
Mael FOSSO <fosso.mael.elvis@gmail.com>
2023-08-24 05:43:10 +0100

    updated the Fail function to return custom error if error

--- utils/utils.go
+++ utils/utils.go
@@ -8,7 +8,8 @@ import (
 func Fail(msg, customError string, err error) error {
 	if err != nil {
 		log.Printf("\n%s: %s", msg, err)
+		return fmt.Errorf(customError)
 	}
 
-	return fmt.Errorf(customError)
+	return nil
 }
