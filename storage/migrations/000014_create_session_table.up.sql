74a82810a17cf3250a0f293e15ee2c3bc35ba1c9
Mael FOSSO <fosso.mael.elvis@gmail.com>
2023-11-12 07:11:42 +0100

    created session table

---
+++ storage/migrations/000014_create_session_table.up.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS sessions (
+  id INTEGER GENERATED ALWAYS AS IDENTITY,
+  start_date TIMESTAMP NOT NULL,
+  end_date TIMESTAMP NOT NULL,
+  organization_id INTEGER NOT NULL,
+  current BOOLEAN DEFAULT FALSE,
+  
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+
+  PRIMARY KEY (id),
+  CONSTRAINT fk_sessions_organizations_organization_id
+    FOREIGN KEY (organization_id) REFERENCES organizations(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
