73e50913239ba04ab8edea01361b7be2c3a6357b
Mael FOSSO <fosso.mael.elvis@gmail.com>
2024-01-25 14:20:42 +0100

    extracting token from cookies

--- services/jwt.go
+++ services/jwt.go
@@ -16,10 +16,10 @@ type contextKey struct {
 	name string
 }
 
-var JwtMemberKey *contextKey
-var JwtClaimsKey *contextKey
-var JwtTokenKey *contextKey
-var JwtErrorKey *contextKey
+var JWTMemberKey *contextKey
+var JWTClaimsKey *contextKey
+var JWTTokenKey *contextKey
+var JWTErrorKey *contextKey
 var jwtSecretKey []byte // (os.Getenv("JWT_SECRET"))
 
 type TWAJWTClaims struct {
@@ -32,10 +32,10 @@ func (k *contextKey) String() string {
 }
 
 func init() {
-	JwtMemberKey = &contextKey{"Member"}
-	JwtClaimsKey = &contextKey{"Claims"}
-	JwtTokenKey = &contextKey{"Token"}
-	JwtErrorKey = &contextKey{"Error"}
+	JWTMemberKey = &contextKey{"Member"}
+	JWTClaimsKey = &contextKey{"Claims"}
+	JWTTokenKey = &contextKey{"Token"}
+	JWTErrorKey = &contextKey{"Error"}
 	jwtSecretKey = []byte(os.Getenv("JWT_SECRET"))
 	// TokenAuth = jwtauth.New("HS256", []byte(os.Getenv("JWT_SECRET")), "s-tschwaa")
 }
@@ -59,18 +59,34 @@ func GenerateJWTToken(data map[string]interface{}) (string, error) {
 	return tokenString, nil
 }
 
-func ExtractTokenFromRequest(r *http.Request) (string, error) {
+func TokenFromCookie(r *http.Request) (string, error) {
+	cookie, err := r.Cookie("jwt")
+	if err != nil {
+		if err == http.ErrNoCookie {
+			return "", fmt.Errorf("no cookie found")
+		} else {
+			return "", fmt.Errorf("error when getting cookies: %w", err)
+		}
+	}
+	if cookie.Value == "" {
+		return "", fmt.Errorf("empty token found")
+	}
+
+	return cookie.Value, nil
+}
+
+func TokenFromHeader(r *http.Request) (string, error) {
 	var tokenString string
 
-	authorizationHeader := r.Header.Get("Authorization")
-	if strings.HasPrefix(authorizationHeader, "Bearer ") {
-		tokenString = strings.TrimPrefix(authorizationHeader, "Bearer ")
+	bearer := r.Header.Get("Authorization")
+	if len(bearer) > 7 && strings.ToUpper(bearer[0:6]) == "BEARER" {
+		tokenString = bearer[7:]
 	} else {
 		return "", fmt.Errorf("no authorization found in header")
 	}
 
 	if tokenString == "" {
-		return "", fmt.Errorf("empty authorization found")
+		return "", fmt.Errorf("empty token found")
 	}
 
 	return tokenString, nil
@@ -78,11 +94,11 @@ func ExtractTokenFromRequest(r *http.Request) (string, error) {
 
 func Verifier(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		tokenString, err := ExtractTokenFromRequest(r)
+		tokenString, err := TokenFromCookie(r)
 
 		ctx := r.Context()
-		ctx = context.WithValue(ctx, JwtTokenKey, tokenString)
-		ctx = context.WithValue(ctx, JwtErrorKey, err)
+		ctx = context.WithValue(ctx, JWTTokenKey, tokenString)
+		ctx = context.WithValue(ctx, JWTErrorKey, err)
 		next.ServeHTTP(w, r.WithContext(ctx))
 	})
 }
@@ -90,8 +106,8 @@ func Verifier(next http.Handler) http.Handler {
 func ParseJWTToken(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
-		tokenString, _ := ctx.Value(JwtTokenKey).(string)
-		err, _ := ctx.Value(JwtErrorKey).(error)
+		tokenString, _ := ctx.Value(JWTTokenKey).(string)
+		err, _ := ctx.Value(JWTErrorKey).(error)
 
 		if err != nil {
 			next.ServeHTTP(w, r)
@@ -107,25 +123,25 @@ func ParseJWTToken(next http.Handler) http.Handler {
 		})
 
 		if err != nil {
-			ctx = context.WithValue(ctx, JwtErrorKey, fmt.Errorf("invalidate token: %v", err))
+			ctx = context.WithValue(ctx, JWTErrorKey, fmt.Errorf("invalidate token: %v", err))
 			next.ServeHTTP(w, r.WithContext(ctx))
 			return
 		}
 
 		if token == nil || !token.Valid {
-			ctx = context.WithValue(ctx, JwtErrorKey, errors.New("invalid token"))
+			ctx = context.WithValue(ctx, JWTErrorKey, errors.New("invalid token"))
 			next.ServeHTTP(w, r.WithContext(ctx))
 			return
 		}
 
 		claims, ok := token.Claims.(jwt.MapClaims)
 		if !ok {
-			ctx = context.WithValue(ctx, JwtErrorKey, errors.New("invalid token claims"))
+			ctx = context.WithValue(ctx, JWTErrorKey, errors.New("invalid token claims"))
 			next.ServeHTTP(w, r.WithContext(ctx))
 			return
 		}
 
-		ctx = context.WithValue(ctx, JwtClaimsKey, claims["user"])
+		ctx = context.WithValue(ctx, JWTClaimsKey, claims["user"])
 		next.ServeHTTP(w, r.WithContext(ctx))
 	})
 }
@@ -133,7 +149,7 @@ func ParseJWTToken(next http.Handler) http.Handler {
 func Authenticator(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
-		err, _ := ctx.Value(JwtErrorKey).(error)
+		err, _ := ctx.Value(JWTErrorKey).(error)
 
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusUnauthorized)
